bf71c84e6243bb5d4997ab95c8237f22f5c0430a
Teo Abril <teo.abril10@gmail.com>
2023-04-14 11:54:09 -0300

    G3-Ej2

---
+++ Ejercicios/Guia_3/ej2.py
@@ -0,0 +1,13 @@
+while True:
+    try:
+        print("Usted va a realizar la division de dos numeros")
+        num1 = float(input("Ingrese el primer numero -->  "))
+        num2 = float(input("Ingrese el segundo numero -->  "))
+        division = num1/num2
+        print(division)
+    except ZeroDivisionError:
+        print("No se puede dividir por cero")
+    finally:
+        cierre = input("Desea continuar (S/N) -->  ")
+        if (cierre.lower()=="n"):
+            break
